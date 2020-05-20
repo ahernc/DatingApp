f0416e7567ffa61e385eace0728937984f74e51d
Colm Ahern <colm.ahern@gmail.com>
2020-05-20 09:52:47 +0100

    L72. Extended User class; Added Photo class

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace DatingApp.API.Models
 {
     public class User
@@ -6,5 +9,19 @@ namespace DatingApp.API.Models
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        // EntityFramework will infer from this that it is a
+        //  one-to-many when it sees ICollection when we create the migration
+        public ICollection<Photo> Photos { get; set; }
+
     }
 }
