5b3e779a0df9ca9647cfb6aec38bc35452b6fe70
Colm Ahern <colm.ahern@gmail.com>
2020-05-25 23:20:50 +0100

    ngx-gallery issue
    Does not matter if it's kolkov  version of ngx-gallery-9, it does not show members page.

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+// import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 // Nice to keep yje angular imports at the top... not absolutely necessary
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -23,50 +24,46 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 
-
 export function tokenGetter() {
-   return localStorage.getItem('token');
+  return localStorage.getItem('token');
 }
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent,
-      MemberListComponent,
-      ListsComponent,
-      MessagesComponent,
-      MemberCardComponent,
-      MemberDetailComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      FormsModule,
-      BrowserAnimationsModule, // L59
-      BsDropdownModule.forRoot(), // followthedocs
-      TabsModule.forRoot(), // L92: Had to import the tabs module manually above... just watch that with some of teh ngxbootstrap stuff.
-      RouterModule.forRoot(appRoutes), // L63
-      JwtModule.forRoot(
-         {
-            // This is how to fix the login issue: the Bearer token is automatically sent up in the next request after login
-            config: {
-               tokenGetter, // slightly different syntax to what you'd expect... tokenGetter: tokenGetter
-               whitelistedDomains: ['localhost:5000'], // L89: This must match where you are sending the token to
-               blacklistedRoutes:  ['localhost:5000/api/auth']
-            }
-         }
-      )
-   ],
-   providers: [
-      AuthService,
-      ErrorInterceptorProvider, // L53
-      MemberDetailResolver, // L93 -- created the resolver.
-      MemberListResolver
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberCardComponent,
+    MemberDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule, // L59
+    BsDropdownModule.forRoot(), // followthedocs
+    TabsModule.forRoot(), // L92: Had to import the tabs module manually above... just watch that with some of teh ngxbootstrap stuff.
+    RouterModule.forRoot(appRoutes), // L63
+    // NgxGalleryModule,
+    JwtModule.forRoot({
+      // This is how to fix the login issue: the Bearer token is automatically sent up in the next request after login
+      config: {
+        tokenGetter, // slightly different syntax to what you'd expect... tokenGetter: tokenGetter
+        whitelistedDomains: ['localhost:5000'], // L89: This must match where you are sending the token to
+        blacklistedRoutes: ['localhost:5000/api/auth'],
+      },
+    }),
+  ],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider, // L53
+    MemberDetailResolver, // L93 -- created the resolver.
+    MemberListResolver,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
